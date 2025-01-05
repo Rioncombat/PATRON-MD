89e5fb33586655bdbea271d599e13dead0f74baa
Rioncombat <rionkombat@gmail.com>
2025-01-05 20:41:37 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "OPAY"
 
-global.banknumber = "8133729715"
+global.banknumber = "8167602071"
 
-global.bankowner = "FADARE"
+global.bankowner = "RIONCOMBAT"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
